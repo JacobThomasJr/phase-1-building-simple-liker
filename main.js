0c2ded92f20335e49b969dd33769ef49c1681123
jacobaranza <jacobthomasjunior@gmail.com>
2023-05-28 08:08:25 -0500

    Building simple liker

--- main.js
+++ main.js
@@ -3,7 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Function to toggle heart
+function toggleHeart(heart) {
+  if (heart.textContent === EMPTY_HEART) {
+    heart.textContent = FULL_HEART;
+    heart.classList.add('activated-heart');
+  } else {
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
 
+// Function to handle heart click event
+function handleHeartClick(event) {
+  const heart = event.target;
+  mimicServerCall()
+    .then(() => {
+      toggleHeart(heart);
+    })
+    .catch((error) => {
+      const errorModal = document.getElementById('error-modal');
+      const errorMessage = errorModal.querySelector('#error-message');
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+// Add click event listener to hearts
+const hearts = document.querySelectorAll('.like-glyph');
+hearts.forEach((heart) => {
+  heart.addEventListener('click', handleHeartClick);
+});
 
 
 
